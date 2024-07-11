22b175e736d0bf14e6d2e457e8e3ef16bb9a7c3e
MColombari <mattia.colombari00@gmail.com>
2024-07-11 10:56:51 +0200

    basic running structure

--- SSTCN/main_process.sh
+++ SSTCN/main_process.sh
@@ -1,16 +1,16 @@
 # written by Bin Sun
 # email: sun.bi@northeastern.edu
 
-mkdir train_videos
-mkdir val_videos
+# mkdir train_videos
+# mkdir val_videos
 ######change path_to_train_videos to your real path for training videos#####################
-mv path_to_train_videos/*color* train_videos/
+# mv path_to_train_videos/*color* train_videos/
 ######change path_to_val_videos to your real path for val videos#####################
-mv path_to_val_videos/*color* val_videos/
+# mv path_to_val_videos/*color* val_videos/
 
 cd data_process
-python wholepose_features_extraction.py --video_path ../train_videos/ --feature_path ../data/train_features --is_train True
-python wholepose_features_extraction.py --video_path ../val_videos/ --feature_path ../data/train_features
+python wholepose_features_extraction.py --video_path /work/cvcs2024/SLR_sentiment_enhanced/datasets/WLASL/WLASL/start_kit/data/train --feature_path ../data/train_features --is_train True
+python wholepose_features_extraction.py --video_path /work/cvcs2024/SLR_sentiment_enhanced/datasets/WLASL/WLASL/start_kit/data/val --feature_path ../data/train_features
 cd ..
 # if you want to delete videos, un common the following command
 #rm -rf train_videos
