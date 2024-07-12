384808702aa1a7402d2444971b3b10930c849fb8
MColombari <mattia.colombari00@gmail.com>
2024-07-12 11:04:19 +0200

    run file changes

--- SSTCN/run_files/run.sh
+++ SSTCN/run_files/run.sh
@@ -9,4 +9,16 @@
 #SBATCH --output="run_output/output.log"
 #SBATCH --error="run_output/error.log"
 
-./main_process.sh
+cd data_process
+python wholepose_features_extraction.py --video_path /work/cvcs2024/SLR_sentiment_enhanced/datasets/WLASL/WLASL/start_kit/data/train --feature_path ../data/train_features --istrain True
+python wholepose_features_extraction.py --video_path /work/cvcs2024/SLR_sentiment_enhanced/datasets/WLASL/WLASL/start_kit/data/val --feature_path ../data/train_features
+    #cd ..
+# if you want to delete videos, un common the following command
+#rm -rf train_videos
+#rm -rf val_videos
+
+####### training #############################
+    #python train_parallel.py --batch_size 160
+###### testing ###########################
+    #python test.py --checkpoint_model /work/cvcs2024/SLR_sentiment_enhanced/model_weights/SSTCN/T_Pose_model_final.pth
+#python test.py --checkpoint_model model_checkpoints/your model
