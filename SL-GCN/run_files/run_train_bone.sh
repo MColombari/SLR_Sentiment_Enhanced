561eecac31c81c8a879a21e1292d0c1da072941c
MColombari <mattia.colombari00@gmail.com>
2024-08-04 14:23:25 +0200

    Train and test sbatch and yaml file SL-GCN

--- SL-GCN/run_files/run_train_bone.sh
+++ SL-GCN/run_files/run_train_bone.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #SBATCH --account=cvcs2024
-#SBATCH --job-name=training_gcn
+#SBATCH --job-name=training_gcn_bone
 #SBATCH --partition=all_usr_prod
 #SBATCH --gres=gpu:2
 #SBATCH --time=24:00:00
 
-#SBATCH --output="run_output/output_bone.log"
-#SBATCH --error="run_output/error_bone.log"
+#SBATCH --output="run_output/output_train_bone.log"
+#SBATCH --error="run_output/error_train_bone.log"
 
 # training 
-python3 main.py --config="config/sign/train/train_bone.yaml"
+python3 main.py --config="config/sign/finetune/train_bone.yaml"
